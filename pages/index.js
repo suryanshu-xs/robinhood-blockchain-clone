0a06d48cc1dcf1173602383bbeb679efbe80c623
Suryanshu <suryanshusweetu12345@gmail.com>
2022-04-09 19:39:57 +0530

    Added Tailwind

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1> Robinhood Blockchain Clone </h1>
+      <h1 className='text-red-600' > Robinhood Blockchain Clone </h1>
     </div>
   )
 }
